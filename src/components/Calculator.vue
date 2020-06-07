77ce4f23f7ea97d1cba74d3c0ba67d49a9aaf9d5
Kevin Leung <kleung@Kevins-MacBook-Pro.local>
2020-06-06 22:44:35 -0600

    Include submit button with validation check, updated CSS

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,57 +1,70 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="2" style="border: 2px solid black" />
-      <v-col cols="8" style="border: 2px solid black">
+      <v-col cols="3" />
+      <v-col cols="6">
         <v-toolbar dense class="title"
           >Total Parenteral Nutrition (TPN) Calculator</v-toolbar
         >
         <v-toolbar dense class="title">Created by Kevin Leung, RDN</v-toolbar>
         <v-card class="mt-2">
-          <FormulateForm class="pa-2 ma-2">
+          <FormulateForm
+            v-model="formValues"
+            class="pa-2 ma-2"
+            @submit="validateForm"
+          >
             <FormulateInput
               type="text"
+              v-model="patientInitials"
               name="patientName"
               label="Patient Initials"
-              placeholder="Ex. J.D."
+              placeholder="Ex. JD"
               class="my-3"
             />
             <FormulateInput
-              type="number"
+              type="text"
+              v-model="patientWeight"
               name="patient weight"
               label="Patient Weight (Kilograms)"
               validation="required|number"
-              placeholder="70 kg"
+              placeholder="Ex. 70 kg"
               class="my-3"
             />
             <FormulateInput
               type="text"
               name="dextrose grams"
-              label="Grams Dextrose"
+              label="Grams Dextrose (required)"
               validation="required|number"
-              placeholder="150 grams"
+              placeholder="Ex. 150 grams..."
               class="my-3"
             />
             <FormulateInput
               type="text"
               name="protein grams"
-              label="Grams Protein"
+              label="Grams Protein (required)"
               validation="required|number"
-              placeholder="100 grams"
+              placeholder="Ex. 100 grams..."
               class="my-3"
             />
             <FormulateInput
               type="text"
               name="lipid grams"
-              label="Grams Lipids"
+              label="Grams Lipids (required)"
               validation="required|number"
-              placeholder="50 grams"
+              placeholder="Ex. 50 grams..."
               class="my-3"
+            />
+            <FormulateInput
+              type="submit"
+              label="Calculate"
+              id="submit-btn"
+              class="purple lighten-1 white--text py-2 my-3"
+              style="border-radius: 5px;"
             />
           </FormulateForm>
         </v-card>
       </v-col>
-      <v-col cols="2" style="border: 2px solid black" />
+      <v-col cols="3" />
     </v-row>
   </v-container>
 </template>
@@ -60,8 +73,33 @@
 export default {
   name: "Calculator",
 
-  data: () => ({}),
+  data: () => ({
+    formValues: {},
+    patientInitials: "",
+    patientWeight: "",
+  }),
 
-  methods: {}
+  methods: {
+    validateForm(data) {
+      alert("data", data);
+    },
+  }
 };
 </script>
+
+<style>
+  .formulate-input-element input {
+    border: 1px solid gray;
+    border-radius: 5px;
+    padding: 10px 2px;
+    width: 100%;
+  }
+  .formulate-input-error {
+    color: #c62828;
+  }
+
+  .formulate-input-element button {
+    width: 100%;
+    font-weight: bold;
+  }
+</style>
