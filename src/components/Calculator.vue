e2ae2a3e624cf0abfeec70bc24e0305ec5144608
Kevin Leung <kleung@Kevins-MacBook-Pro.local>
2020-06-13 21:13:07 -0600

    Add v-footer w/ text, update App.vue structure

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,125 +1,127 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="3" />
-      <v-col cols="6">
-        <v-toolbar dense class="title">
-          Total Parenteral Nutrition (TPN) Calculator
-        </v-toolbar>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="3" />
+        <v-col cols="6">
+          <v-toolbar dense class="title">
+            Total Parenteral Nutrition (TPN) Calculator
+          </v-toolbar>
 
-        <v-card class="mt-4">
-          <v-alert :value="true" type="warning">
-            TPN Calculations are based off a Dextrose 70% Stock Solution, 10%
-            Amino Acid Stock Solution, and 20% Lipid Stock Solution. As always,
-            please double check your work for any errors.
-          </v-alert>
+          <v-card class="mt-4">
+            <v-alert :value="true" type="warning">
+              TPN Calculations are based off a Dextrose 70% Stock Solution, 10%
+              Amino Acid Stock Solution, and 20% Lipid Stock Solution. As
+              always, please double check your work for any errors.
+            </v-alert>
 
-          <FormulateForm
-            v-model="formValues"
-            class="pa-2 mx-2"
-            @submit="validateForm"
-          >
-            <v-row>
-              <v-col>
-                <FormulateInput
-                  type="text"
-                  v-model="patientInitials"
-                  name="patientName"
-                  label="Patient Initials (Optional)"
-                  placeholder="Ex. JD"
-                  class="my-3"
-                />
-              </v-col>
-              <v-col>
-                <FormulateInput
-                  type="text"
-                  name="weight"
-                  label="Patient Weight (Kilograms)"
-                  validation="required|number"
-                  placeholder="Ex. 70 kg"
-                  class="my-3"
-                />
-              </v-col>
-            </v-row>
-            <FormulateInput
-              type="text"
-              name="dextrose"
-              label="Grams Dextrose (required) / (70% Stock Solution)"
-              validation="required|number|max:1000,value"
-              placeholder="Ex. 150 grams..."
-              class="my-3"
-            />
-            <FormulateInput
-              type="text"
-              name="protein"
-              label="Grams Protein (required) / (10% Stock Solution)"
-              validation="required|number|max:1000,value"
-              placeholder="Ex. 100 grams..."
-              class="my-3"
-            />
-            <FormulateInput
-              type="text"
-              name="lipid"
-              label="Grams Lipids (required) / (20% Stock Solution)"
-              validation="required|number|max:1000,value"
-              placeholder="Ex. 50 grams..."
-              class="my-3"
-            />
-            <FormulateInput
-              type="text"
-              name="hours"
-              label="TPN Duration (Hours)"
-              validation="required|number|max:24,value"
-              placeholder="Ex. 24 hours..."
-              class="my-3"
-            />
-            <v-row>
-              <v-col>
-                <FormulateInput
-                  type="submit"
-                  label="Calculate"
-                  id="submit-btn"
-                  class="primary darken white--text py-2 my-3"
-                  style="border-radius: 5px;"
-                />
-              </v-col>
-              <v-col>
-                <FormulateInput
-                  type="button"
-                  label="Reset"
-                  id="reset-btn"
-                  class="error darken-1 white--text py-2 my-3"
-                  style="border-radius: 5px;"
-                  @click="resetForm"
-                />
-              </v-col>
-            </v-row>
-          </FormulateForm>
-        </v-card>
-      </v-col>
-      <v-col cols="3" />
-    </v-row>
+            <FormulateForm
+              v-model="formValues"
+              class="pa-2 mx-2"
+              @submit="validateForm"
+            >
+              <v-row>
+                <v-col>
+                  <FormulateInput
+                    type="text"
+                    v-model="patientInitials"
+                    name="patientName"
+                    label="Patient Initials (Optional)"
+                    placeholder="Ex. JD"
+                    class="my-3"
+                  />
+                </v-col>
+                <v-col>
+                  <FormulateInput
+                    type="text"
+                    name="weight"
+                    label="Patient Weight (Kilograms)"
+                    validation="required|number"
+                    placeholder="Ex. 70 kg"
+                    class="my-3"
+                  />
+                </v-col>
+              </v-row>
+              <FormulateInput
+                type="text"
+                name="dextrose"
+                label="Grams Dextrose (required) / (70% Stock Solution)"
+                validation="required|number|max:1000,value"
+                placeholder="Ex. 150 grams..."
+                class="my-3"
+              />
+              <FormulateInput
+                type="text"
+                name="protein"
+                label="Grams Protein (required) / (10% Stock Solution)"
+                validation="required|number|max:1000,value"
+                placeholder="Ex. 100 grams..."
+                class="my-3"
+              />
+              <FormulateInput
+                type="text"
+                name="lipid"
+                label="Grams Lipids (required) / (20% Stock Solution)"
+                validation="required|number|max:1000,value"
+                placeholder="Ex. 50 grams..."
+                class="my-3"
+              />
+              <FormulateInput
+                type="text"
+                name="hours"
+                label="TPN Duration (Hours)"
+                validation="required|number|max:24,value"
+                placeholder="Ex. 24 hours..."
+                class="my-3"
+              />
+              <v-row>
+                <v-col>
+                  <FormulateInput
+                    type="submit"
+                    label="Calculate"
+                    id="submit-btn"
+                    class="primary darken white--text py-2 my-3"
+                    style="border-radius: 5px;"
+                  />
+                </v-col>
+                <v-col>
+                  <FormulateInput
+                    type="button"
+                    label="Reset"
+                    id="reset-btn"
+                    class="error darken-1 white--text py-2 my-3"
+                    style="border-radius: 5px;"
+                    @click="resetForm"
+                  />
+                </v-col>
+              </v-row>
+            </FormulateForm>
+          </v-card>
+        </v-col>
+        <v-col cols="3" />
+      </v-row>
 
-    <v-row>
-      <v-col cols="3" />
-      <v-col cols="6">
-        <v-card v-if="isCalculated">
-          <v-card-title>{{ patientCardTitle }}'s TPN Summary</v-card-title>
-          <v-divider />
+      <v-row>
+        <v-col cols="3" />
+        <v-col cols="6">
+          <v-card v-if="isCalculated">
+            <v-card-title>{{ patientCardTitle }}'s TPN Summary</v-card-title>
+            <v-divider />
 
-          <v-card-text class="title">
-            {{ patientCardTitle }} will be receiving a total of
-            {{ formatNumber(totalCalories) }} calories within a
-            {{ formValues.hours }} hour duration. TPN will provide
-            {{ caloriesPerKilogram }} kcals/kg,
-            {{ proteinPerKilogram }} g/protein/kg, with a glomerular filtration
-            rate (GFR) of {{ gfr }} mg/kg/hr.
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="3" />
-    </v-row>
-  </v-container>
+            <v-card-text class="title">
+              {{ patientCardTitle }} will be receiving a total of
+              {{ formatNumber(totalCalories) }} calories within a
+              {{ formValues.hours }} hour duration. TPN will provide
+              {{ caloriesPerKilogram }} kcals/kg,
+              {{ proteinPerKilogram }} g/protein/kg, with a glomerular
+              filtration rate (GFR) of {{ gfr }} mg/kg/hr.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="3" />
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -210,6 +212,7 @@ export default {
   border-radius: 5px;
   padding: 10px 2px;
   width: 100%;
+  height: 100%;
 }
 .formulate-input-error {
   color: #c62828;
@@ -220,6 +223,10 @@ export default {
   .formulate-input-element {
   border: 1px solid #c62828;
   border-radius: 6px;
+}
+
+.formulate-input-wrapper {
+  height: 100%;
 }
 
 .formulate-input-element button {
